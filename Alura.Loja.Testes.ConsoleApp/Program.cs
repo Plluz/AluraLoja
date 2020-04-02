2c16bba40f0f6837b3a5f54ee3625812ba7b8e2c
Pedrinho <plluz@outlook.com>
2020-04-02 00:57:35 -0300

    feat: crud do produto

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -11,11 +11,58 @@ namespace Alura.Loja.Testes.ConsoleApp
         static void Main(string[] args)
         {
             //GravarUsandoAdoNet();
-            GravarUsandoEntity();
+            GravarProduto();
+            RecuperarProdutos();
+            AtualizarProduto();
+            RecuperarProdutos();
+            ExcluirProdutos();
+            RecuperarProdutos();
+
+            Console.ReadLine();
         }
 
-        private static void GravarUsandoEntity()
+        private static void AtualizarProduto()
         {
+            Console.WriteLine("Atualizando dados...");
+            using (var db = new LojaContext())
+            {
+                var produto = db.Produtos.FirstOrDefault();
+                produto.Preco = 14.90;
+                db.Produtos.Update(produto);
+                db.SaveChanges();
+            }
+        }
+
+        private static void ExcluirProdutos()
+        {
+            Console.WriteLine("Excluindo dados...");
+            using (var db = new LojaContext())
+            {
+                IList<Produto> produtos = db.Produtos.ToList();
+                foreach (var item in produtos)
+                {
+                    db.Produtos.Remove(item);
+                }
+                db.SaveChanges();
+            }
+        }
+
+        private static void RecuperarProdutos()
+        {
+            Console.WriteLine("Obtendo dados...");
+            using (var loja = new LojaContext())
+            {
+                IList<Produto> produtos = loja.Produtos.ToList();
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item.Nome + " - " + item.Preco);
+                }
+            }
+        }
+
+        private static void GravarProduto()
+        {
+            Console.WriteLine("Gravando dados...");
             Produto p = new Produto();
             p.Nome = "Harry Potter e a Ordem da Fênix";
             p.Categoria = "Livros";
